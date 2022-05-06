ae00e364af8a7bd26e6efc4a4974a5fb078ad1f8
mdiktushar <md.tushar1804@gmail.com>
2022-05-06 16:24:32 +0600

    migration and relationship

--- 2-API-Development-Using-Sanctum-Authentication/app/Models/User.php
+++ 2-API-Development-Using-Sanctum-Authentication/app/Models/User.php
@@ -41,4 +41,10 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function UserInformation(Type $var = null)
+    {
+        # code...
+        return $this->hasOne(UserInformation::class);
+    }
 }
