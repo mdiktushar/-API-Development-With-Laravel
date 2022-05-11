1a67c0b69c1e57269d8ca36ca906ec1a02782d8c
mdiktushar <md.tushar1804@gmail.com>
2022-05-12 00:30:46 +0600

    model

---
+++ 4-API-Development-Using-Passport-Authentication/app/Http/Controllers/UserController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function register(Request $request)
+    {
+        # code...
+    }
+    
+    public function login(Request $request)
+    {
+        # code...
+    }
+    public function profile(Type $var = null)
+    {
+        # code...
+    }
+    public function logout(Type $var = null)
+    {
+        # code...
+    }
+}
